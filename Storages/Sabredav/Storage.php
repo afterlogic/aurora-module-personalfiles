6d30f9f2124aa7b714a5e21eb8a0a73f67a42865
Vasil Sokolov <vasil@afterlogic.com>
2023-01-23 23:22:05 +0200

    Updated dates in copyright

--- Storages/Sabredav/Storage.php
+++ Storages/Sabredav/Storage.php
@@ -25,7 +25,7 @@ use function Sabre\Event\Loop\instance;
 /**
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
- * @copyright Copyright (c) 2019, Afterlogic Corp.
+ * @copyright Copyright (c) 2023, Afterlogic Corp.
  *
  * @internal
  *
--- Storages/Storage.php
+++ Storages/Storage.php
@@ -10,7 +10,7 @@ namespace Aurora\Modules\PersonalFiles\Storages;
 /**
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
- * @copyright Copyright (c) 2019, Afterlogic Corp.
+ * @copyright Copyright (c) 2023, Afterlogic Corp.
  */
 class Storage extends \Aurora\System\Managers\AbstractStorage
 {
